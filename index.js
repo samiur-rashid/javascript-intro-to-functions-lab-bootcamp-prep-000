938c7b8f2193ff00573d5a6b8d0887b44b00ab93
SAMIUR RASHID <samrashid@gmail.com>
2017-10-14 14:35:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,13 +7,16 @@ function whisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var uppercase = "HELLO", lowercase = "hello"
+  var uppercase = "HELLO", lowercase = "hello", loveYou = "I love you, Grandma"
 
   if (string === lowercase) {
     return "I can't hear you!"
   }
   else if (string === uppercase) {
     return "YES INDEED!"
+  }
+  else if (string === loveYou) {
+    return "I love you, too"
   }
 
 }
